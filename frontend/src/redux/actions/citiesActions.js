ed99537e0f50c1a6678d3d73ecea2487effce9c5
micaela-torre <micaelatorre@outlook.es>
2021-10-20 22:59:06 -0300

    git

--- frontend/src/redux/actions/citiesActions.js
+++ frontend/src/redux/actions/citiesActions.js
@@ -3,9 +3,9 @@ import axios from "axios";
 const citiesActions = {
   getAllCities: (token) => {
     return async (dispatch, getState) => {
-      let respuesta = await axios.get("https://mytinerary-web.herokuapp.com/api/cities");
+      let respuesta = await axios.get("https://mytinerary-web.herokuapp.com/api/cities"); 
       let info = respuesta.data.response;
-      if (!respuesta.data.success) {
+      if (!respuesta.data.success) { 
         throw new Error();
       }
       dispatch({ type: "GET_ALL_CITIES", payload: info });
